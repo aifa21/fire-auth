d9b2d07f4ff6d2f0a70472f18eb5ca20c2568230
aifa21 <37891597+aifa21@users.noreply.github.com>
2020-09-13 20:40:38 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBTw4AzKGFMe3oCgsE3c3Gn1MHCl8UiCAk",
+    authDomain: "ema-john-simple-6552c.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-6552c.firebaseio.com",
+    projectId: "ema-john-simple-6552c",
+    storageBucket: "ema-john-simple-6552c.appspot.com",
+    messagingSenderId: "271672226252",
+    appId: "1:271672226252:web:45b764713c049463f3ad69",
+    measurementId: "G-5BHVXZT4Q2"
+  };
+  export default firebaseConfig;
